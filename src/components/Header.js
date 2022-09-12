de2381b554a3fac3996977eb3668b05927282acb
claudeando <claude.mm6@gmail.com>
2022-09-12 20:15:03 +0100

    minor change

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+function Header() {
+    return (
+        <tr>
+            <th>id</th>
+            <th>image</th>
+            <th>name</th>
+            <th>audienceType</th>
+            <th>status</th>
+            <th>organisation</th>
+            <th>region</th>
+            <th>campaignId</th>
+            <th>campaignName</th>
+            <th>watchers</th>
+            <th>dueDate</th>
+            <th>lastModifiedDate</th>
+            <th>lastModifiedBy</th>
+            <th>createdDate</th>
+            <th>createdBy</th>
+            <th>comments</th>
+            <th>type</th>
+            <th>workOrderId</th>
+            <th>sceneCount</th>
+        </tr>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,68 @@
+import Data from '../data.json';
+
+function Main() {
+    return (
+        <>
+            {
+                Data && Data.content.map((datum) =>
+                    <tr>
+                        <td>{datum.id}</td>
+                        <td>{datum.image}</td>
+                        <td>{datum.name}</td>
+                        <td>{datum.audienceType}</td>
+                        <td>{datum.status}</td>
+
+                        <td>
+                            {
+                                datum.organisation !== null &&
+                                <>
+                                    <td>{datum.organisation.id}</td>
+                                    <td>{datum.organisation.name}</td>
+                                </>
+                            }
+                        </td>
+
+                        <td>{datum.region}</td>
+                        <td>{datum.campaignId}</td>
+                        <td>{datum.campaignName}</td>
+
+                        <td>
+                            {
+                                datum.watchers !== null && datum.watchers.length > 0 && datum.watchers.map((watcher) => {
+                                    return <>{watcher}</>
+                                }
+                                )}
+                        </td>
+
+                        <td>{datum.dueDate}</td>
+                        <td>{datum.lastModifiedDate}</td>
+                        <td>{datum.lastModifiedBy}</td>
+                        <td>{datum.createdDate}</td>
+                        <td>{datum.createdBy}</td>
+
+                        {<td>
+                            {
+                                datum.comments !== null && datum.comments.length > 0 && datum.comments.map((comment) => {
+                                    return (
+                                        <>
+                                            <td>{comment.id}</td>
+                                            <td>{comment.text}</td>
+                                            <td>{comment.createdDate}</td>
+                                            <td>{comment.createdBy}</td>
+                                        </>
+                                    )
+                                }
+                                )}
+                        </td>}
+
+                        <td>{datum.type}</td>
+                        <td>{datum.workOrderId}</td>
+                        <td>{datum.sceneCount}</td>
+                    </tr>
+                )
+            }
+        </>
+    )
+}
+
+export default Main;
